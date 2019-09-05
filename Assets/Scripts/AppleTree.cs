c1a259ac47fb64ca430772d4b92c07caf53fcc67
JoeTruelove <43154676+JoeTruelove@users.noreply.github.com>
2019-09-05 12:46:47 -0400

    Added tree, apple and basket prefabs. Added material and started script... adjusted camera
    Created apple tree and apple and basket. Created materials for those objects. Adjusted camera. Started Apple tree Scripting. Apple tree moves left and right and doesn't go past the bounderies

---
+++ Assets/Scripts/AppleTree.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+
+    public GameObject applePrefab;
+
+    public float speed = 1f;
+
+    public float leftAndRightEdge = 22;
+
+    public float chanceToChangeDirections = 0.1f;
+
+    public float secondsBetweenAppleDrops = 1f;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);
+        }
+    }
+}
